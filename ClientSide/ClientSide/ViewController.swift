b27eeafb9a3aa578cecf902a21d72043a548da96
Kevia <cloud@kevia.me>
2016-04-20 19:14:16 -0700

    Save Point

--- ClientSide/ClientSide/ViewController.swift
+++ ClientSide/ClientSide/ViewController.swift
@@ -8,12 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController
+class ViewController: UIViewController, UITextFieldDelegate
 {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var currentLocationField: UITextField!
+    @IBOutlet weak var destinationLocationField: UITextField!
+    
+    @IBOutlet var textfields: [UITextField]!
+    
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
+    
+    @IBAction func sendRequest(sender: UIButton)
+    {
+        
+    }
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        for textfield in textfields
+        {
+            textfield.delegate = self
+        }
+        
+        submitButton.layer.cornerRadius = submitButton.frame.size.width / 2
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool
+    {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField)
+    {
+        animateViewMoving(true, moveValue: 75)
+    }
+    func textFieldDidEndEditing(textField: UITextField)
+    {
+        animateViewMoving(false, moveValue: 75)
+    }
+    
+    func animateViewMoving (up: Bool, moveValue : CGFloat)
+    {
+        let movementDuration:NSTimeInterval = 0.3
+        let movement:CGFloat = up ? -moveValue : moveValue
+        UIView.beginAnimations( "animateView", context: nil)
+        UIView.setAnimationBeginsFromCurrentState(true)
+        UIView.setAnimationDuration(movementDuration )
+        self.view.frame = CGRectOffset(self.view.frame, 0,  movement)
+        UIView.commitAnimations()
     }
 }
 
