24d8d4d1ee005e21d9278ab243ce9b5eba30522f
Kevia <cloud@kevia.me>
2016-04-20 17:39:44 -0700

    Started Project

---
+++ ClientSide/ClientSide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClientSide
+//
+//  Created by Ace_Rimmer on 4/20/16.
+//  Copyright © 2016 Ace_Rimmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
+
